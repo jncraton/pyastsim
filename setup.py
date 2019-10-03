5bb035260928d2d9479461090662df1e7b2918f6
Jon Craton <jncraton@gmail.com>
2019-10-03 12:17:13 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyastsim",
+    version="1.0.3",
+    author="Jon Craton",
+    author_email="jon@joncraton.com",
+    description="Detect similarities between Python source files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jncraton/pyastsim",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'pyastsim=pyastsim:main',
+        ],
+    },
+)
